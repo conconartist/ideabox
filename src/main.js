0238c5b814f0299a0154d931c52c7f626b85fcae
Alia Peterson <alia@pnwebs.com>
2020-10-28 17:42:23 -0600

    Add new card functionality

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ var ideaArray = []
 
 var inputTitle = document.querySelector('#input-title')
 var inputBody = document.querySelector('#input-body')
+var userIdeas = document.querySelector('.user-ideas')
+var ideaCardTemplate = document.querySelector('#unique-card')
 
 var saveButton = document.querySelector('#button-save')
 
@@ -11,25 +13,47 @@ var saveButton = document.querySelector('#button-save')
 // event listeners
 
 saveButton.addEventListener('click', saveNewIdea)
-inputBody.addEventListener('keyup', enableSaveButton)
-inputTitle.addEventListener('keyup', enableSaveButton)
+inputBody.addEventListener('keyup', setSaveButtonState)
+inputTitle.addEventListener('keyup', setSaveButtonState)
 
 // functions
 
 function saveNewIdea() {
-  var newIdea = new Idea(inputTitle.value, inputBody.textarea)
+  var newIdea = new Idea(inputTitle.value, inputBody.value)
   ideaArray.push(newIdea)
-  
+
+  createCardFromTemplate(newIdea)
   clearInputFields()
+  setSaveButtonState()
 }
 
-function enableSaveButton() {
+function setSaveButtonState() {
   if (inputTitle.value !== '' && inputBody.value !== '') {
     saveButton.disabled = false
+  } else {
+    saveButton.disabled = true
   }
 }
 
 function clearInputFields() {
   inputTitle.value = ''
-  inputBody.textarea = ''
+  inputBody.value = ''
+}
+
+function createCardFromTemplate(userIdea) {
+  var ideaCard = ideaCardTemplate.content.cloneNode(true)
+
+  addTitleToTemplate(ideaCard, userIdea.title)
+  addBodyToTemplate(ideaCard, userIdea.body)
+
+  userIdeas.appendChild(ideaCard)
+  clearInputFields()
+}
+
+function addTitleToTemplate(card, userTitle) {
+  card.querySelector('div.card-body h3').innerText = userTitle
+}
+
+function addBodyToTemplate(card, userBody) {
+  card.querySelector('div.card-body p').innerText = userBody
 }
