e5aaae2f893e0cad2a05d90426187e598b4e9e55
Josh Aragon <josharagon99@gmail.com>
2020-10-29 20:11:25 -0600

    Add delete idea functionality

--- src/main.js
+++ src/main.js
@@ -7,14 +7,22 @@ var inputBody = document.querySelector('#input-body')
 var userIdeas = document.querySelector('.user-ideas')
 var ideaCardTemplate = document.querySelector('#unique-card')
 
-var saveButton = document.querySelector('#button-save')
-
+var saveButton = document.querySelector('#button-save');
+var deleteIdea = document.querySelector('.icon-delete')
+var favIdea = document.querySelector('.icon-star')
 
 // event listeners
-
 saveButton.addEventListener('click', saveNewIdea)
 inputBody.addEventListener('keyup', setSaveButtonState)
 inputTitle.addEventListener('keyup', setSaveButtonState)
+
+userIdeas.addEventListener('click', function(event) {
+  if (event.target.className === 'icon-delete') {
+    removeCardDisplay(event.target.id)
+    removeCardFromArray()
+  }
+});
+
 
 // functions
 
@@ -42,12 +50,26 @@ function clearInputFields() {
 
 function createCardFromTemplate(userIdea) {
   var ideaCard = ideaCardTemplate.content.cloneNode(true)
-
   addTitleToTemplate(ideaCard, userIdea.title)
   addBodyToTemplate(ideaCard, userIdea.body)
+  addIdToTemplate(ideaCard, userIdea.id)
 
   userIdeas.appendChild(ideaCard)
   clearInputFields()
+}
+
+function removeCardDisplay(id) {
+  var cardToDelete = document.getElementById(id)
+  userIdeas.removeChild(cardToDelete)
+}
+
+function removeCardFromArray() {
+  var delIdea = event.target.id;
+  for (var i = 0; i < ideaArray.length; i++){
+    if(delIdea == ideaArray[i].id){
+      ideaArray.splice(i, 1);
+    }
+  }
 }
 
 function addTitleToTemplate(card, userTitle) {
@@ -56,4 +78,9 @@ function addTitleToTemplate(card, userTitle) {
 
 function addBodyToTemplate(card, userBody) {
   card.querySelector('div.card-body p').innerText = userBody
+}
+
+function addIdToTemplate(card, cardId) {
+  card.querySelector('section.idea-card').id = cardId
+  card.querySelector('img.icon-delete').id = cardId
 }
